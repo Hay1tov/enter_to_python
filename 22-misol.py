1791fa98334c36cbbc6617dd721a0b2c7c584b4b
Hay1tov <eldorh35@gmail.com>
2024-12-23 22:58:32 +0500

    Bajarildi

---
+++ 22-misol.py
@@ -0,0 +1,8 @@
+price = float(input("Mahsulot narxini kiriting: "))
+discount = float(input("Chegirma foizini kiriting: "))
+
+s = price * (discount / 100)
+
+result = price - s
+
+print("Chegirma narx:", result)
