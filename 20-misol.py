0c207c6d1a228a43c053d6a10e04d01bd135ea2d
Hay1tov <eldorh35@gmail.com>
2024-12-23 22:44:09 +0500

    Bajarildi

---
+++ 20-misol.py
@@ -0,0 +1,7 @@
+a = int(input('sonni kiriting >> '))
+b = int(input('nechinchi darajasini chiqarish kerak >> '))
+
+# sonlarni darajasini hisoblashda "pow" funksiya sidan foydalanamiz
+s = pow(a, b)
+
+print(s)
