b85839526086934ac0f5200e6cf745d42bf164c5
Hay1tov <eldorh35@gmail.com>
2024-12-23 22:39:01 +0500

    Bajarildi

---
+++ 19-misol.py
@@ -0,0 +1,8 @@
+name = input("Ismingizni kiriting: ")
+surname = input("Familiyangizni kiriting: ")
+
+
+print(name + surname)
+print(surname + name)
+print(name.lower() + surname.lower())
+print(surname.lower() + name.lower())
